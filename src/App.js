d51adc81141c124a36cb212a643b45da6a79ff0c
shreya kondilya <skondilya@gmail.com>
2020-07-22 01:18:40 -0700

    adding files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      text:"",
+      author:""
+    }
+    this.generateRandomQuote = this.generateRandomQuote.bind(this);
+  }
+  componentWillMount(){
+    this.generateRandomQuote();
+  }
+  generateRandomQuote(){
+      const RandomNumber=function(max){
+        return Math.floor(Math.random()*Math.floor(max))
+      };
+    fetch('https://type.fit/api/quotes')
+    .then((Response)=> Response.json())
+    .then((data)=>{
+          var quote=data[RandomNumber(data.length-1)].text;
+          var author="";
+          if(data[RandomNumber(data.length-1)].author==null){
+            author="Unknown";
+            } else{
+            author=data[RandomNumber(data.length-1)].author;
+          }
+          this.setState({
+          text:quote,
+          author:author
+          })
+    }).catch((err)=>console.log('Error is '+err))
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>The Quote Machine</h1>
+        <div id="quote-box" className="border ">
+          <div id="quote-text" className="text-sm-center">
+          <i className="fa fa-quote-left quote-icon"> </i><span> {this.state.text}</span></div>
+          <div id="author-div">- {this.state.author}</div>
+          <button id="new-quote" onClick={this.generateRandomQuote}>
+            New Quote
+          </button>
+          <button>
+          <a
+              href={`https://twitter.com/intent/tweet?text="${this.state.text}" %0D%0A- ${this.state.author}%0D%0A`}
+              id="tweet-quote"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fa fa-twitter twitter"/>
+            </a>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
